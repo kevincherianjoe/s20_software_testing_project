95c8e0a85492f33f1a22527b5a9b587576cbdd79
Jason <randd7@yahoo.com>
2020-04-12 09:57:57 -0500

    Added another Junit test for more coverage

---
+++ fusion_chord/src/test/PingPredecessorThreadTest.java
@@ -0,0 +1,57 @@
+package test;
+
+import chordfusion.ChordNode;
+import chordfusion.PingPredecessorThread;
+import chordfusion.ServerDataTable;
+import org.junit.Test;
+
+import java.net.InetSocketAddress;
+
+public class PingPredecessorThreadTest {
+
+    @Test
+    public void pptt1() {
+        InetSocketAddress iNetSock;
+        InetSocketAddress iNetSockPred;
+        ChordNode chordNode;
+        ServerDataTable serverDataTable;
+
+        iNetSockPred = new InetSocketAddress("127.0.0.1", 1025);
+        iNetSock = new InetSocketAddress("127.0.0.1", 1026);
+        serverDataTable = ServerDataTable.getServerTable(
+                "serverTable.txt", 1, "C");
+        chordNode = new ChordNode(iNetSock, serverDataTable);
+        chordNode.setPredecessor(iNetSockPred);
+        PingPredecessorThread pingPredecessorThread = new PingPredecessorThread(chordNode);
+        pingPredecessorThread.start();
+    }
+
+    @Test
+    public void pptt2() {
+        InetSocketAddress iNetSock;
+        InetSocketAddress iNetSockPred;
+        ChordNode chordNode;
+        ServerDataTable serverDataTable;
+        chordNode = null;
+        PingPredecessorThread pingPredecessorThread = new PingPredecessorThread(chordNode);
+        pingPredecessorThread.start();
+    }
+
+    @Test
+    public void pptt3() {
+        InetSocketAddress iNetSock;
+        InetSocketAddress iNetSockPred;
+        ChordNode chordNode;
+        ServerDataTable serverDataTable;
+
+//        iNetSockPred = new InetSocketAddress("127.0.0.1", 1025);
+        iNetSock = new InetSocketAddress("127.0.0.1", 1026);
+        serverDataTable = ServerDataTable.getServerTable(
+                "serverTable.txt", 1, "C");
+        chordNode = new ChordNode(iNetSock, serverDataTable);
+        chordNode.setPredecessor(null);
+        PingPredecessorThread pingPredecessorThread = new PingPredecessorThread(chordNode);
+        pingPredecessorThread.start();
+    }
+
+}
