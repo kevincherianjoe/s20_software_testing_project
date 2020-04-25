e908f8b53c2a753332ec30a2a7697001cd7e5dd1
AM <arnobmallick@gmail.com>
2020-04-24 21:23:51 -0500

    started on mockito tests for ChordServer

--- fusion_chord/src/test/junit/ChordServerTest.java
+++ fusion_chord/src/test/junit/ChordServerTest.java
@@ -8,7 +8,8 @@ import chordfusion.ChordServer;
 public class ChordServerTest {
 
     // test no args
-    @Test(timeout = 1) public void cst0() {
+    @Test(timeout = 1)
+    public void cst0() {
         // ChordServer.main(new String[]{});
 
         // untestable w/ JUnit only
@@ -16,7 +17,8 @@ public class ChordServerTest {
     }
 
     // test one arg
-    @Test(timeout = 1) public void cst1() {
+    @Test(timeout = 1)
+    public void cst1() {
         // ChordServer.main(new String[]{ "test-input" });
 
         // untestable w/ JUnit only
@@ -24,20 +26,23 @@ public class ChordServerTest {
     }
 
     // test two args - invalid file path
-    @Test(expected = NullPointerException.class) public void cst2() {
+    @Test(expected = NullPointerException.class)
+    public void cst2() {
         ChordServer.main(new String[]{ "invalid-path", "0" });
         assertTrue(false);
     }
 
     // test two args - invalid node id
-    @Test(expected = NumberFormatException.class) public void cst3() {
+    @Test(expected = NumberFormatException.class)
+    public void cst3() {
         String path = System.getProperty("user.dir") + "/../serverTable.txt";
         ChordServer.main(new String[]{ path, "not-an-int" });
         assertTrue(false);
     }
 
     // test two args - valid input
-    @Test(timeout = 1) public void cst4() {
+    @Test(timeout = 1)
+    public void cst4() {
         String path = System.getProperty("user.dir") + "/../serverTable.txt";
         ChordServer.main(new String[]{ path, "1" });
 
--- fusion_chord/src/test/mockito/ChordServerTest.java
+++ fusion_chord/src/test/mockito/ChordServerTest.java
@@ -1,5 +1,72 @@
 package test.mockito;
 
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import chordfusion.ChordNode;
+import chordfusion.ChordServer;
+import chordfusion.ServerDataTable;
+import chordfusion.ServerDataTable.ServerData;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ ServerDataTable.class, ChordServer.class })
 public class ChordServerTest {
 
+    // test invalid server data table file path
+    @Test(expected = NullPointerException.class)
+    public void cst01() {
+        PowerMockito.mockStatic(ServerDataTable.class);
+        when(ServerDataTable.getServerTable(anyString(), anyInt(), anyString()))
+            .thenReturn(new ServerDataTable());
+        ServerDataTable sdtMock = mock(ServerDataTable.class);
+        when(sdtMock.getMyServer()).thenReturn(null);
+
+        ChordServer.main(new String[]{ "invalid-path", "0" });
+        assertTrue(false);
+    }
+
+    // test empty ServerDataTable & immediate exit
+    @Test
+    public void cst02() throws Exception {
+        String path = System.getProperty("user.dir") + "/../serverTable.txt";
+        String ipAddr = "ipaddr";
+        int ipPort = 10;
+
+        PowerMockito.mockStatic(ServerDataTable.class);
+        ServerDataTable sdtMock = mock(ServerDataTable.class);
+        ServerData sdMock = mock(ServerData.class);
+        ChordNode cnMock = mock(ChordNode.class);
+        when(ServerDataTable.getServerTable(anyString(), anyInt(), anyString()))
+            .thenReturn(sdtMock);
+        when(sdtMock.getMyServer())
+            .thenReturn(sdMock);
+        when(sdMock.getPort())
+            .thenReturn(ipPort);
+        when(sdMock.getIpAddress())
+            .thenReturn(ipAddr);
+        when(sdtMock.getServerDataTable())
+            .thenReturn(new ArrayList<ServerData>());
+        PowerMockito.whenNew(ChordNode.class).withAnyArguments().thenReturn(cnMock);
+        when(cnMock.join(any()))
+            .thenReturn(true);
+
+        String in01 = "exit";
+        InputStream in = new ByteArrayInputStream(in01.getBytes());
+        System.setIn(in);
+
+        ChordServer.main(new String[]{ path, "1" });
+        assertTrue(false);
+    }
 }
