13987b04149aa8cd72eca9422948b4740888bde8
Jason <randd7@yahoo.com>
2020-04-25 20:34:28 -0500

    Improved code coverage 21/26

--- fusion_chord/src/test/mockito/StabilizeThreadTest.java
+++ fusion_chord/src/test/mockito/StabilizeThreadTest.java
@@ -1,48 +1,72 @@
 package test.mockito;
 
 import chordfusion.ChordNode;
+import chordfusion.MessageConstants;
 import chordfusion.ServerDataTable;
 import chordfusion.StabilizeThread;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 
 import java.net.InetSocketAddress;
+
+import static org.mockito.Mockito.mock;
 
 
 public class StabilizeThreadTest {
 
     @Mock
     private ChordNode m_chordNode;
-    @Mock
-    private ServerDataTable m_serverDataTable;
     @InjectMocks
+    private ServerDataTable m_serverDataTable;
+
     private StabilizeThread m_stabilizeThread;
 
     @Before
     public void setup() {
 
-        InetSocketAddress iNetSock;
-
-        iNetSock = new InetSocketAddress("127.0.0.1", 1025);
-        m_chordNode = new ChordNode(iNetSock, m_serverDataTable);
+        m_chordNode = mock(ChordNode.class);
         m_stabilizeThread = new StabilizeThread(m_chordNode);
     }
 
     @Test
-    public void stt0() {
+    public void stt1() {
 
-//        InetSocketAddress successor = new InetSocketAddress();
+        InetSocketAddress iNetSockSucc;
 
-//        for (int i = 0; i < 2; i++) {
-//            InetSocketAddress inetSocketAddress = new InetSocketAddress("127.0.0.1", 1025 + i);
-//            m_chordNode.join(inetSocketAddress);
-//        }
-
-//        Mockito.when(m_chordNode.getSuccessor()).thenReturn(successor);
-
+        iNetSockSucc = new InetSocketAddress("127.0.0.1", 1027);
+        Mockito.when(m_chordNode.getSuccessor()).thenReturn(iNetSockSucc);
         m_stabilizeThread.start();
     }
 
+    @Test
+    public void stt2() {
+        //catches else & line 62
+
+        InetSocketAddress iNetSockSucc;
+
+        iNetSockSucc = new InetSocketAddress("127.0.0.1", 1028);
+        Mockito.when(m_chordNode.getSuccessor()).thenReturn(iNetSockSucc);
+        Mockito.when(m_chordNode.sendReceiveAddress(iNetSockSucc, MessageConstants.GET_PREDECESSOR)).thenReturn(iNetSockSucc);
+        m_stabilizeThread.start();
+    }
+
+    @Test
+    public void stt3() {
+
+        InetSocketAddress iNetSockPred;
+        InetSocketAddress inetSocketCurr;
+        InetSocketAddress iNetSockSucc;
+
+        iNetSockSucc = new InetSocketAddress("127.0.0.1", 8030);
+        Mockito.when(m_chordNode.getSuccessor()).thenReturn(iNetSockSucc);
+        iNetSockPred = new InetSocketAddress("127.0.0.1", 8031);
+        inetSocketCurr = new InetSocketAddress("127.0.0.1", 8040);
+        Mockito.when(m_chordNode.sendReceiveAddress(iNetSockSucc, MessageConstants.GET_PREDECESSOR)).thenReturn(iNetSockPred);
+        Mockito.when(m_chordNode.getMyAddress()).thenReturn(inetSocketCurr);
+        m_stabilizeThread.start();
+//        m_stabilizeThread.run();
+    }
 }
