51467805078a9b62cbd41159f40fd8707652eb35
Jason <randd7@yahoo.com>
2020-04-26 09:59:50 -0500

    Cleaned up code

--- fusion_chord/src/test/mockito/MessageRouterTest.java
+++ fusion_chord/src/test/mockito/MessageRouterTest.java
@@ -2,51 +2,65 @@ package test.mockito;
 
 import chordfusion.ChordNode;
 import chordfusion.MessageRouter;
-import chordfusion.ServerDataTable;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.InjectMocks;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
+import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+import static org.mockito.Mockito.mock;
+
+@RunWith(MockitoJUnitRunner.class)
 public class MessageRouterTest {
+
 
     @Mock
     private ChordNode m_chordNode;
     @Mock
-    private ServerDataTable m_serverDataTable;
-    @InjectMocks
+    private ServerSocket m_serverSocket;
+    @Mock
+    private Socket m_Socket;
+
     MessageRouter m_messageRouter;
 
     @Before
     public void setup() {
 
-        InetSocketAddress iNetSock;
-
-        iNetSock = new InetSocketAddress("127.0.0.1", 1026);
-        m_chordNode = new ChordNode(iNetSock, m_serverDataTable);
+        m_chordNode = mock(ChordNode.class);
         m_messageRouter = new MessageRouter(m_chordNode);
     }
 
     @Test
-    public void mrt0() {
+    public void mrt2() {
 
-        InetSocketAddress iNetSockPred;
+        InetSocketAddress iNetSockAdd;
 
-        iNetSockPred = new InetSocketAddress("127.0.0.1", 1025);
-        m_chordNode.setPredecessor(iNetSockPred);
-//        MessageRouter messageRouter = new MessageRouter(m_chordNode);
+        iNetSockAdd = new InetSocketAddress("127.0.0.1", 8040);
+        Mockito.when(m_chordNode.getMyAddress()).thenReturn(iNetSockAdd);
+        try {
+            m_serverSocket = new ServerSocket();
+            m_serverSocket.bind(iNetSockAdd);
+            m_Socket = new Socket("127.0.0.1", 8040);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         m_messageRouter.start();
-
     }
 
     @Test
-    public void mrt1() {
+    public void mrt3() {
 
-        m_chordNode.setPredecessor(null);
-//        m_messageRouter = new MessageRouter(m_chordNode);
+        InetSocketAddress iNetSockAdd;
+
+        iNetSockAdd = new InetSocketAddress("127.0.0.1", 8032);
+        Mockito.when(m_chordNode.getMyAddress()).thenReturn(iNetSockAdd);
+        MessageRouter messageRouter = new MessageRouter(m_chordNode);
         m_messageRouter.start();
-
     }
 }
